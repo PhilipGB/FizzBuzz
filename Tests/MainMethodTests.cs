781f826aa1516fcafd4ee1fcfa012854108e46c8
Philip Burgess <philipgrahamburgess@gmail.com>
2023-11-27 10:32:55 +0000

    Failing test cases added for expected output

--- Tests/MainMethodTests.cs
+++ Tests/MainMethodTests.cs
@@ -49,5 +49,49 @@ public class MainMethodTests
 
         //Assert
         Assert.That(actual, Is.EqualTo(expected));
-    }    
+    }
+
+    [TestCase(0, "1")]
+    [TestCase(2, "Fizz")]
+    [TestCase(4, "Buzz")]
+    [TestCase(6, "7")]
+    [TestCase(14, "FizzBuzz")]
+    [TestCase(99, "Buzz")]
+    public void TestMainOutputNoArgs(int index, string value)
+    {
+        //Arrange
+        StringWriter output = new ();
+        Console.SetOut(output);
+
+        var expected = value;
+
+        //Act
+        Program.Main(Array.Empty<string>());
+        var outputArray = output
+            .ToString()
+            .TrimEnd('\n') // End of Console output is '\n'
+            .Split("\n");
+
+        var actual = outputArray[index];
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [TestCase("11")]
+    public void TestMainOutputWithArgs(string count)
+    {
+        //Arrange
+        StringWriter output = new ();
+        Console.SetOut(output);
+
+        var expected = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\n";
+
+        //Act
+        Program.Main(new string[] { count });
+        var actual  = output.ToString();
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
