ef8fe80fc1c01f544ba208637baf07a0a1c43610
Philip Burgess <philipgrahamburgess@gmail.com>
2023-11-27 10:45:20 +0000

    Divisible by 3 tests passed

--- FizzBuzz/Extensions/IntExtensionMethods.cs
+++ FizzBuzz/Extensions/IntExtensionMethods.cs
@@ -3,6 +3,11 @@ public static class IntExtensionMethods
 {
     public static string ToFizzBuzzValue(this int value)
     {
+        if (value % 3 == 0)
+        {
+            return "Fizz";
+        }
+
         return value.ToString();
     }
 }
