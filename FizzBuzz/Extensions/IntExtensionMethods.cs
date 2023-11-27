47c2d061e1279af8bb03ca474c78db4022a00670
Philip Burgess <philipgrahamburgess@gmail.com>
2023-11-27 10:52:42 +0000

    All tests passed

--- FizzBuzz/Extensions/IntExtensionMethods.cs
+++ FizzBuzz/Extensions/IntExtensionMethods.cs
@@ -3,6 +3,11 @@ public static class IntExtensionMethods
 {
     public static string ToFizzBuzzValue(this int value)
     {
+        if (value % 15 == 0)
+        {
+            return "FizzBuzz";
+        }
+
         if (value % 3 == 0)
         {
             return "Fizz";
